398c6fab3f0241e9392d71d9e13371d2faa50566
Aswin Adi Saputra <aswin.adisaputra@gmail.com>
2022-10-31 17:44:54 +0700

    .

--- fic_assignment_1/android/app/src/main/kotlin/com/example/fic_assignment_1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fic_assignment_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- fugi_fic_1/android/app/src/main/kotlin/com/example/fugi_fic_1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fugi_fic_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
