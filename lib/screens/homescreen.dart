915af496ef3cd8a200e3ecaa7543a46eaa44091a
Aswin Adi Saputra <aswin.adisaputra@gmail.com>
2022-11-14 09:04:25 +0700

    UI Slicing

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -13,153 +13,118 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SizedBox(
-          child: ListView(
-            children: [
-              Container(
-                height: 57.6,
-                // margin: EdgeInsets.only(top: 28.8, right: 28.8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      height: 57.6,
-                      width: 57.6,
-                      padding: const EdgeInsets.all(14.4),
-                      child: const Icon(Icons.menu),
-                    ),
-                    Container(
-                      height: 57.6,
-                      width: 200,
-                      padding: const EdgeInsets.all(14.4),
-                      child: Center(
-                        child: Text(
-                          "Home",
-                          style: GoogleFonts.poppins(fontSize: 16),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      height: 57.6,
-                      width: 57.6,
-                      padding: const EdgeInsets.all(14.4),
-                      child: const Icon(Icons.search),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 14.4, left: 14.4),
-                child: Text(
-                  "Discover the most\nmodern furniture",
-                  style: GoogleFonts.poppins(
-                    fontSize: 22,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 28.8, left: 14.4),
-                child: SizedBox(
-                  height: 28.8,
-                  child: ListView(
-                    // padding: EdgeInsets.only(left: 28.8, top: 28.8),
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      SizedBox(
-                        height: 28.8,
-                        width: 50,
-                        child: Text(
-                          "All",
-                          style: GoogleFonts.poppins(
-                              fontSize: 12, color: Colors.black),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 28.8,
-                        width: 80,
-                        child: Text(
-                          "Living Room",
-                          style: GoogleFonts.poppins(
-                              fontSize: 12, color: Colors.black),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 28.8,
-                        width: 20,
-                      ),
-                      SizedBox(
-                        height: 28.8,
-                        width: 80,
-                        child: Text(
-                          "Bedroom",
-                          style: GoogleFonts.poppins(
-                              fontSize: 12, color: Colors.black),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 28.8,
-                        width: 10,
-                      ),
-                      SizedBox(
-                        height: 28.8,
-                        width: 80,
-                        child: Text(
-                          "Dining Room",
-                          style: GoogleFonts.poppins(
-                              fontSize: 12, color: Colors.black),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 28.8,
-                        width: 20,
-                      ),
-                      SizedBox(
-                        height: 28.8,
-                        width: 80,
-                        child: Text(
-                          "Kitchen",
-                          style: GoogleFonts.poppins(
-                              fontSize: 12, color: Colors.black),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(14.4),
-                child: SizedBox(
-                  height: 28.8,
-                  child: Text(
-                    "Recommended Furnitures",
-                    style: GoogleFonts.poppins(
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ),
-              GridView(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                ),
-                children: <Widget>[
-                  FlutterLogo(),
-                  FlutterLogo(),
-                  FlutterLogo(),
-                  FlutterLogo(),
-                ],
-              ),
-            ],
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: const Padding(
+          padding: EdgeInsets.only(left: 14.4),
+          child: Icon(Icons.menu),
+        ),
+        title: Center(
+          child: Text(
+            'Home',
+            style: GoogleFonts.poppins(fontSize: 16),
           ),
         ),
+        actions: [
+          const Padding(
+            padding: EdgeInsets.only(right: 14.4),
+            child: Icon(Icons.search),
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
-          Icon(
-            Icons.home,
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: '',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: '',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '',
           ),
         ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Discover the most modern furniture
+          Padding(
+            padding: const EdgeInsets.only(left: 14.4),
+            child: Text(
+              "Discover the most\nmodern furniture",
+              style: GoogleFonts.poppins(fontSize: 22),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          // Horizontal list view
+          SizedBox(
+            height: 28.8,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 14.4),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  const SizedBox(
+                    // height: 28.8,
+                    width: 70,
+                    child: Text('All'),
+                  ),
+                  const SizedBox(
+                    // height: 28.8,
+                    width: 80,
+                    child: Text('Living Room'),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  const SizedBox(
+                    // height: 28.8,
+                    width: 80,
+                    child: Text('Bedroom'),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  const SizedBox(
+                    // height: 28.8,
+                    width: 80,
+                    child: Text('Dining Room'),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  const SizedBox(
+                    // height: 28.8,
+                    width: 80,
+                    child: Text('Kitchen'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 14.4),
+            child: Text('Recommended furniture'),
+          ),
+        ],
+
+        // Recommended furniture
       ),
     );
   }
